2e8efb62d7a5a487b47edcb31cc48acb534f1971
Aleksander <50501127+thealexgregor@users.noreply.github.com>
2021-08-15 23:52:49 +0400

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
+exports.min = function min(array) {
+    if (array && array.length > 0) {
+        let min = array[0];
+        for (let item of array) {
+            min > item ? (min = item) : (min = min);
+        }
+        return min;
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array && array.length > 0) {
+        let max = array[0];
+        for (let item of array) {
+            max < item ? (max = item) : (max = max);
+        }
+        return max;
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array && array.length > 0) {
+        return array.reduce((acc, current) => acc + current) / array.length;
+    } else {
+        return 0;
+    }
+};
